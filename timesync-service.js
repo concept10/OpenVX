b7a8603c30b4ea18c85cc35f363b125e51fb7383
concept10 <tenthconcept@gmail.com>
2020-08-10 15:35:45 -0500

    Adding timesync-service.js.

---
+++ timesync-service.js
@@ -0,0 +1,18 @@
+
+
+var express = require('express');
+var timesyncServer = require('timesync/server');
+
+var port = 8081;
+var app = express();
+
+
+
+app.listen(port);
+console.log('Server listening at http://localhost:' + port);
+ 
+// serve static index.html
+app.get('/', express.static(__dirname));
+ 
+// handle timesync requests
+app.use('/timesync', timesyncServer.requestHandler);
